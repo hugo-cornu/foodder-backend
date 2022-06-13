60d8587b6149d26784bccdef2b69ccc0c0a78814
Sébastien R <103518570+sebastienrxs@users.noreply.github.com>
2022-06-13 17:39:04 +0200

    added verify route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,9 @@
-const bcrypt = require("bcryptjs");
-const jsonwebtoken = require("jsonwebtoken");
-const User = require("../models/User.model");
+const bcrypt = require("bcryptjs")
+const jsonwebtoken = require("jsonwebtoken")
+const User = require("../models/User.model")
 
-const router = require("express").Router();
-const saltRounds = 10;
+const router = require("express").Router()
+const saltRounds = 10
 
 /*
   GET /signup
@@ -11,11 +11,11 @@ const saltRounds = 10;
   */
 router.get("/signup", async (req, res, next) => {
   res.status(200).json({message: "Signup Page Connected!"})
-  
+
   // const root = __dirname.replace("routes", "");
   // console.log(root);
   // res.sendFile("views/auth/signup.html", { root });
-});
+})
 
 /*
   POST /signup
@@ -26,58 +26,79 @@ router.post("/signup", async (req, res, next) => {
     console.log(">>>>>>>>>>", req.body)
     // const userCreated = await User.create(userToCreate)
     // res.status(201).json({message: "User Created", userCreated})
-    
-    const { username, password} = req.body;
-    const foundUser = await User.findOne({ username });
+
+    const {username, password} = req.body
+    const foundUser = await User.findOne({username})
     if (foundUser) {
       res
         .status(401)
-        .json({ message: "Username already exists. Try logging in instead." });
-      return;
+        .json({message: "Username already exists. Try logging in instead."})
+      return
     }
 
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(saltRounds)
+    const hashedPassword = await bcrypt.hash(password, salt)
 
     const createdUser = await User.create({
       ...req.body,
       password: hashedPassword,
-    });
+    })
 
-    res.status(201).json(createdUser);
-  
-  }  catch (error) {
-    console.log(error);
-    next(error);
+    res.status(201).json(createdUser)
+  } catch (error) {
+    console.log(error)
+    next(error)
   }
-});
+})
 
 /* POST /login
 Logging the user into our website
 */
 router.post("/login", async (req, res, next) => {
-  const { username, password } = req.body;
-  const foundUser = await User.findOne({ username });
+  const {username, password} = req.body
+  const foundUser = await User.findOne({username})
 
   if (!foundUser) {
-    res.status(404).json({ message: "username does not exist" });
-    return;
+    res.status(404).json({message: "username does not exist"})
+    return
   }
 
-  const isPasswordMatched = await bcrypt.compare(password, foundUser.password);
+  const isPasswordMatched = await bcrypt.compare(password, foundUser.password)
   if (!isPasswordMatched) {
-    res.status(401).json({ message: "password does not match" });
-    return;
+    res.status(401).json({message: "password does not match"})
+    return
   }
 
-  const payload = {username, name: foundUser.name, _id: foundUser._id };
+  const payload = {username, name: foundUser.name, _id: foundUser._id}
 
   const authToken = jsonwebtoken.sign(payload, process.env.TOKEN_SECRET, {
     algorithm: "HS256",
     expiresIn: "15s",
-  });
+  })
 
-  res.status(200).json({ isLoggedIn: true, authToken });
-});
+  res.status(200).json({isLoggedIn: true, authToken})
+})
+
+router.get("/verify", async (req, res, next) => {
+  // Verify the bearer token is still valid
+  // get the bearer token from the header
+  const {authorization} = req.headers
+
+  // isolate the jwt
+  const token = authorization.replace("Bearer ", "")
+  console.log({token})
+
+  try {
+    // verify the jwt with the jsonwebtoken package
+    const payload = jsonwebtoken.verify(token, process.env.TOKEN_SECRET)
+    console.log({payload})
+
+    // send the user the payload
+    res.json({token, payload})
+  } catch (error) {
+    console.error(error)
+    res.status(400).json({message: "Invalid token"})
+  }
+})
 
 module.exports = router
