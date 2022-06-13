f6c33ea3f33cf26000d9a148130dcb7f71e2f595
Hugo Cornu <cornu.hugo.p@gmail.com>
2022-06-13 11:06:34 +0200

    rotues updates + models updates

--- routes/articles.routes.js
+++ routes/articles.routes.js
@@ -2,9 +2,7 @@ const router = require("express").Router();
 const isAuthenticated = require("../middleware/isAuthenticated");
 const Article = require("../models/Article.model");
 
-
 // ------------------ FEED PAGE ------------------ //
-
 
 // GET ALL POSTS IN THE FEED PAGE
 router.get("/", isAuthenticated, async (req, res, next) => {
@@ -16,8 +14,6 @@ router.get("/", isAuthenticated, async (req, res, next) => {
 });
 
 // ------------------ PROFILE PAGE ------------------ //
-
-
 
 // GET POST FROM A PROFILE PAGE -> FILTER IF USER != PageOwner (Only private posts)
 
@@ -32,37 +28,36 @@ router.get("/", isAuthenticated, async (req, res, next) => {
 
 // POST - CREATE A NEW POST
 router.post("/", isAuthenticated, async (req, res, next) => {
-    try {
-      const articleToCreate = req.body;
-      const articleCreated = await Article.create(articleToCreate);
-      res.status(201).json(articleCreated);
-    } catch (error) {
-      next(error);
-    }
-  });
-  
-  // PATCH - UPDATE A NEW POST
-  router.patch("/:id", isAuthenticated, async (req, res, next) => {
-    try {
-      await Article.findByIdAndUpdate(req.params.id, req.body);
-      res.status(200).json({ message: `Good job, you updated ${req.params.id}` });
-    } catch (error) {
-      next(error);
-    }
-  });
-  
-  // DELETE A POST
-  router.delete("/:id", isAuthenticated, async (req, res, next) => {
-    try {
-      await Article.findByIdAndDelete(req.params.id);
-      res.status(200).json({ message: `Good job, you deleted ${req.params.id}` });
-    } catch (error) {
-      next(error);
-    }
-  });
+  try {
+    const articleToCreate = req.body;
+    const articleCreated = await Article.create(articleToCreate);
+    res.status(201).json(articleCreated);
+  } catch (error) {
+    next(error);
+  }
+});
 
+// PATCH - UPDATE A NEW POST
+router.patch("/:id", isAuthenticated, async (req, res, next) => {
+  try {
+    await Article.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({ message: `Good job, you updated ${req.params.id}` });
+  } catch (error) {
+    next(error);
+  }
+});
 
-  // ------------------ GENERAL BEHAVIOUR ------------------ //
+// DELETE A POST
+router.delete("/:id", isAuthenticated, async (req, res, next) => {
+  try {
+    await Article.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: `Good job, you deleted ${req.params.id}` });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// ------------------ GENERAL BEHAVIOUR ------------------ //
 
 // GET ONE POST (FULL PAGE) WHEN A USER CLICK ON A POST
 router.get("/:id", isAuthenticated, async (req, res, next) => {
@@ -73,7 +68,5 @@ router.get("/:id", isAuthenticated, async (req, res, next) => {
     next(error);
   }
 });
-
-
 
 module.exports = router;
