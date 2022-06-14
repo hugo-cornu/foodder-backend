24d0a62a81ea88045834c439d629fad4d6cdc202
Hugo Cornu <cornu.hugo.p@gmail.com>
2022-06-14 12:16:16 +0200

    Country Model done and countries seeding too

---
+++ models/Country.model.js
@@ -0,0 +1,7 @@
+const { Schema, SchemaTypes, model } = require("mongoose")
+const countryModel = new Schema({
+  countryName: String,
+  countryCca3: String,
+})
+const Country = model("Country", countryModel)
+module.exports = Country
--- seeds/countries.seed.js
+++ seeds/countries.seed.js
@@ -1,12 +1,24 @@
 const axios = require("axios")
-seedCountries()
+const Country = require("../models/Country.model")
+getCountries()
 
-async function seedCountries() {
+async function getCountries() {
   const response = await axios.get("https://restcountries.com/v3.1/all")
-  console.log("data:", response.data)
-  return response.data
-  console.log("response:", response)
+  // console.log("data:", response.data)
+  const allCountries = response.data
+  const allCountriesCleaned = allCountries.map((country) => {
+    return { countryName: country.name.common, countryCca3: country.cca3 }
+  })
+  seedCountries(allCountriesCleaned)
 }
 
-// const newList = res.map((element) => {
-//   element
+async function seedCountries(allCountriesCleaned) {
+  const newConnection = await require("../db/index.js")
+  const createdCountries = await Country.create(allCountriesCleaned)
+  await Promise.all(allCountriesCleaned)
+
+  console.log("createdCountries:", createdCountries)
+  console.log(`Created ${createdCountries.length} countries.`)
+  await mongoose.connection.close()
+  console.log("Connection closed.")
+}
