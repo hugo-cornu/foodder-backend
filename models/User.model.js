021805e5c029946c28d31c1bdc75be4aa6fed6ef
Hugo Cornu <cornu.hugo.p@gmail.com>
2022-06-15 11:05:30 +0200

    Create form validation for : 1- strong password, 2- username+email+password are mandatory to signup, 3- username + email already exist in the database. try to logging instead

--- models/User.model.js
+++ models/User.model.js
@@ -8,8 +8,11 @@ const userSchema = new Schema(
     },
     username: {
       type: String,
+      required: [true, "Email is required"],
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
       unique: true,
-      required: true,
+      lowercase: true,
+      trim: true,
     },
     email: {
       type: String,
