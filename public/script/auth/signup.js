b4f6d9d9f59cc509d3c72a23e32c9945aa1874d5
Hugo Cornu <cornu.hugo.p@gmail.com>
2022-06-17 12:34:36 +0200

    Signup is better -> All fiels are are -> the API is working as the user can signup. We retrieve the image URL in cloudinary

--- public/script/auth/signup.js
+++ public/script/auth/signup.js
@@ -1,15 +1,22 @@
 const signupForm = document.querySelector("form"),
+  fullNameInput = signupForm.querySelector("#name"),
   usernameInput = signupForm.querySelector("#username"),
+  emailInput = signupForm.querySelector("#email"),
+  fileInput = signupForm.querySelector("#profile-picture"),
   passwordInput = signupForm.querySelector("#password"),
-  baseUrl = "http://localhost:3000/auth"
+  baseUrl = "http://localhost:3000/api/auth"
 
 signupForm.addEventListener("submit", handleSubmit)
 
 async function handleSubmit(e) {
   e.preventDefault()
-  const username = usernameInput.value,
-    password = passwordInput.value,
-    userToCreate = { username, password }
+  const userToCreate = new FormData()
+  userToCreate.append("name", fullNameInput.value)
+  userToCreate.append("username", usernameInput.value)
+  userToCreate.append("email", emailInput.value)
+  userToCreate.append("image", fileInput.files[0])
+  userToCreate.append("password", passwordInput.value)
+
   try {
     const response = await axios.post(`${baseUrl}/signup`, userToCreate)
     console.log(response)
