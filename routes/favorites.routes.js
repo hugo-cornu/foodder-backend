a69e1b6d6132b9469ecc30b6a0b9ad65fdba9c38
Sébastien R <103518570+sebastienrxs@users.noreply.github.com>
2022-06-17 11:47:00 +0200

    change fav route to make it into one

--- routes/favorites.routes.js
+++ routes/favorites.routes.js
@@ -1,36 +1,60 @@
 const router = require("express").Router()
 const isAuthenticated = require("../middleware/isAuthenticated")
 const Favorite = require("../models/Favorite.model")
+const Article = require("../models/Article.model")
 
-// POST - CREATE A FAVORITE
+// POST CREATE OR DELETE FAVORITE
 router.post("/", isAuthenticated, async (req, res, next) => {
   try {
-    const favToCreate = req.body
-    favToCreate.user = req.user._id
-    const favCreated = await Favorite.create(favToCreate)
-    res.status(201).json(favCreated)
-  } catch (error) {
-    next(error)
-  }
-})
+    const favToCheck = req.body
+    favToCheck.user = req.user._id
 
-// DELETE A FAVORITE
-router.delete("/", isAuthenticated, async (req, res, next) => {
-  const userId = req.user._id.toString()
-  console.log(">>>userId:", userId)
-  const foundFav = await Favorite.findById(req.body.id)
-  console.log(">>>foundFav:", foundFav)
-  const favAuthor = foundFav.user.toString()
-  console.log(">>>favAuthor:", favAuthor)
+    // get ID of article to check
+    const favToCheckId = req.body.article
 
-  if (userId !== favAuthor) {
-    res.status(401).json({ message: "You are not the author of the favorite" })
-    return
-  }
+    // get all likes by user
+    const allFavOfUser = await Favorite.find({ user: req.user._id }).select(
+      "article"
+    )
 
-  try {
-    await Favorite.findByIdAndDelete(foundFav)
-    res.status(200).json({ message: `You deleted the favorite ${foundFav}` })
+    // return array of  strings of  articles' IDs liked by user
+    const allFavOfUserIds = await allFavOfUser.map((item) => {
+      return item.article.toString()
+    })
+
+    // check if new fav is included
+    const doesFavExist = allFavOfUserIds.includes(favToCheckId.toString())
+
+    if (!doesFavExist) {
+      // if fav doesn't exist, create new fav
+      const favCreated = await Favorite.create(favToCheck)
+      res.status(201).json({ "Created favorite": favCreated })
+    } else {
+      //delete fav if it exists
+      const userId = req.user._id.toString()
+      const foundFav = await Favorite.findOne({
+        article: favToCheckId,
+        user: userId,
+      })
+      const favAuthor = foundFav.user.toString()
+
+      //check if user is author of fav
+      if (userId !== favAuthor) {
+        res
+          .status(401)
+          .json({ message: "You are not the author of the favorite" })
+        return
+      }
+
+      try {
+        await Favorite.findByIdAndDelete(foundFav._id)
+        res
+          .status(200)
+          .json({ message: `You deleted the favorite ${foundFav}` })
+      } catch (error) {
+        next(error)
+      }
+    }
   } catch (error) {
     next(error)
   }
