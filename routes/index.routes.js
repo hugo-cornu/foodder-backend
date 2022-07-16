15e122eb4d8579f1ab11095411eaf6d644fc33f7
Sébastien R <103518570+sebastienrxs@users.noreply.github.com>
2022-07-16 17:04:21 +0200

    delete html views

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,16 +1,9 @@
 const router = require("express").Router()
 
 router.get("/", (req, res, next) => {
-  res.sendFile("views/main.html", { root: require("../root") })
+  res.json(
+    "Welcome to Foodder app. Read the API's documentation: https://documenter.getpostman.com/view/21219252/UzBiPULQ"
+  )
 })
-
-// router.get("/", (req, res, next) => {
-//   res.json(
-//     "Welcome to Foodder app. Read the API's documentation: https://documenter.getpostman.com/view/21219252/UzBiPULQ"
-//   )
-// })
-
-// You put the next routes here 👇
-// example: router.use("/auth", authRoutes)
 
 module.exports = router
